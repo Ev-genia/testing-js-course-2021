2c2cbd2773112300574ea07f5343d71fe3160f74
Eugenia Kostareva <eugenia@kostarev.local>
2021-04-07 15:39:15 +0300

    new commit

--- 1. Basics/HomeWork01/removeFalsyValues.js
+++ 1. Basics/HomeWork01/removeFalsyValues.js
@@ -1,6 +1,6 @@
-export default class Numbers{
+export default class Numbers {
     removeFalsyValues(array) { 
-        if (!Array.isArray(array)){
+        if (!Array.isArray(array)) {
             throw new Error('Argument should be an array');
         }
         return array.filter(val => !!val);  
