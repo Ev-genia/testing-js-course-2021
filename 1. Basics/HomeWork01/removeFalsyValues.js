490894f821aeb03cf7151adbb64754b0fbcb37a0
Eugenia Kostareva <eugenia@kostarev.local>
2021-04-07 15:05:31 +0300

    Add second test

--- 1. Basics/HomeWork01/removeFalsyValues.js
+++ 1. Basics/HomeWork01/removeFalsyValues.js
@@ -1,5 +1,8 @@
 export default class Numbers{
     removeFalsyValues(array) { 
+        if (!Array.isArray(array)){
+            throw new Error('Argument should be an array');
+        }
         return array.filter(val => !!val);  
     }
 }
--- 1. Basics/HomeWork01/removeFalsyValues.test.js
+++ 1. Basics/HomeWork01/removeFalsyValues.test.js
@@ -1,14 +1,24 @@
+import Numbers from './removeFalsyValues.js';
 import removeFalsyValues from './removeFalsyValues.js';
 
 test('Felsy values remove from arrey', () => {
-    // Arrenge
+    // Arrange
     const array = [45, '', 0, 'hello', 4.7, false];
     const expectedResult = [45, 'hello', 4.7];
+    const numbers = new Numbers();
 
     // Act
-    result = new removeFalsyValues(array);
+    const result = numbers.removeFalsyValues(array);
 
     // Assert
     expect(result).toEqual(expectedResult);
-
 })
+
+test('Argument should be an array', () => {
+    // Arrange
+    const numbers = new Numbers();
+
+    // Act and Assert
+    expect(() => numbers.removeFalsyValues('Hello')).toThrow();
+    })
+
