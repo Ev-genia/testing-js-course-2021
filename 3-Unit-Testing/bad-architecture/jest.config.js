e674c0274ecd3ec0ac731719801b14641c3c716f
Eugenia Kostareva <eugenia@kostarev.local>
2021-04-21 14:24:19 +0500

    add bad arch

---
+++ 3-Unit-Testing/bad-architecture/jest.config.js
@@ -0,0 +1,11 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/en/configuration.html
+ */
+
+module.exports = {
+  clearMocks: true,
+  // coverageProvider: "v8",
+  // testEnvironment: "node",
+  setupFilesAfterEnv: ["./tests/setupTests.js"],
+};
---
+++ 3-Unit-Testing/bad-architecture/tests/bad-archutecture.test.js
@@ -0,0 +1,68 @@
+// import { TestScheduler } from '@jest/core';
+import { deleteIssue } from '../src/main';
+
+describe('function deleteIssue', () => {
+    test('Delete is correctly', () => {
+        // Arrange
+        expect.hasAssertions();
+        const ID_1 = '0ae8cc16-be04-5741-a1be-d6745d73665f';
+        const ID_2 = '92698cbe-4eb0-5727-9959-d587d3042e94';
+        const ID_3 = 'b5d9e628-f8d2-5e7e-8da0-93d2f70164eb';
+        const fakeDataFirstCall = [
+            {
+                id: ID_1,
+                description: "jbj",
+                severity: "Low",
+                assignedTo: "jbk",
+                status: "Open"
+            },
+            {
+                id: ID_2,
+                description: "fdbcxfbxf",
+                severity: "High",
+                assignedTo: "1234",
+                status: "Open"
+            },
+            {
+                id: ID_3,
+                description: "dfkghfkl",
+                severity: "Medium",
+                assignedTo: "vbjkdvkjdzv",
+                status: "Open"
+            }
+        ];
+        const fakeDataSecondCall = [
+            {
+                id: ID_1,
+                description: "jbj",
+                severity: "Low",
+                assignedTo: "jbk",
+                status: "Open"
+            },
+            {
+                id: ID_2,
+                description: "fdbcxfbxf",
+                severity: "High",
+                assignedTo: "1234",
+                status: "Open"
+            }
+        ];
+        JSON.parse = jest.fn().mockReturnValueOnce(fakeDataFirstCall).mockReturnValueOnce(fakeDataSecondCall);
+
+        // Act
+        deleteIssue(ID_3);
+
+        // Assert
+        expect(fakeDataFirstCall).toEqual(fakeDataSecondCall);
+    })
+});
+
+describe('setStatusClosed function', () => {
+    test('Close status is correctly', () => {
+        // Arrange
+
+        // Act
+
+        // Asseart
+    });
+});
